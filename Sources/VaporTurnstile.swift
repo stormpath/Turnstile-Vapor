8d4156a231c77195c05ba3f337559adde39bb35d
Edward Jiang <edward@stormpath.com>
2016-07-26 23:25:28 -0700

    added hack to make singleton work for now

--- Sources/VaporTurnstile.swift
+++ Sources/VaporTurnstile.swift
@@ -10,13 +10,13 @@ import Vapor
 import Turnstile
 
 public class VaporTurnstile: Provider {
-    var turnstile: Turnstile
+    public let turnstile: Turnstile
     
     public init() {
         turnstile = Turnstile(sessionManager: VaporSessionManager())
     }
     
     public func boot(with droplet: Droplet) {
-        
+        Turnstile.sharedTurnstile = turnstile // HACK - fix sometime
     }
 }
