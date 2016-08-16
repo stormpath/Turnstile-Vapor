ddae881f2bb571e41dcdba8c5d332d4ad0864405
Edward Jiang <edward@stormpath.com>
2016-08-15 17:52:46 -0700

    point to public Turnstile repo

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporTurnstile",
     dependencies: [
-        .Package(url: "../Turnstile", majorVersion: 0),
+        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16)
     ]
 )
